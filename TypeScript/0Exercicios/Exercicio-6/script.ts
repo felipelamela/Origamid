bbf5acd2f20aae09105dc54d3bd2aceaf062972f
felipelamela <fe-fl@outlook.com>
2023-06-12 14:47:30 -0300

    exercios 5/6

---
+++ TypeScript/0Exercicios/Exercicio-6/script.ts
@@ -0,0 +1,19 @@
+/*
+1 - Selecione os elementos com a classe link.
+2 - Crie uma função que deve ser executada para cada elemento.
+3 - Modificar através da função o estilo da color e border.
+*/
+
+const links = document.querySelectorAll(".link");
+
+function modificaLink(links: NodeList, color: string, border: string) {
+  const linksArray = Array.from(links);
+  linksArray.forEach((link) => {
+    if (link instanceof HTMLElement) {
+      link.style.color = `#${color}`;
+      link.style.border = `${border}`;
+    }
+  });
+}
+
+modificaLink(links, "ff9933", "3px solid red");
